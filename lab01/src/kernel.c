0e06f2e26cda4f33d0f419ccd42e85dc78296abd
AkiraZheng <1428384878@qq.com>
2025-09-14 08:37:40 -0700

    ldr/str lab1:ldr地址偏移

--- lab01/src/kernel.c
+++ lab01/src/kernel.c
@@ -1,9 +1,19 @@
 #include "uart.h"
 
+extern void ldr_test(void);
+
+void my_ldr_test(void)
+{
+	ldr_test(); // Call the external assembly function
+}
+
 void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Welcome BenOS!\r\n");
+
+	//test of ldr
+	my_ldr_test();
 
 	while (1) {
 		uart_send(uart_recv());
